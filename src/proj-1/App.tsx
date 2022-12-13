4565c12374c8bbe381284a922517d411da63a9ff
Philipp Khromov <filooloog@mail.ru>
2022-12-13 22:45:22 +0300

    feat: add TextSection map to App

--- src/proj-1/App.tsx
+++ src/proj-1/App.tsx
@@ -1,9 +1,15 @@
 import Info from './commons/Info';
+import TextSection from './commons/TextSection';
+import { sections } from './config/data';
 
 function App() {
+  const renderSections = () =>
+    sections.map((section) => <TextSection {...section} key={section.title} />);
+
   return (
     <div>
       <Info />
+      {renderSections()}
     </div>
   );
 }
---
+++ src/proj-1/config/data.ts
@@ -0,0 +1,14 @@
+import { TextSectionProps } from '../commons/TextSection';
+
+const sections: TextSectionProps[] = [
+  {
+    title: 'About',
+    text: 'This is a text about me',
+  },
+  {
+    title: 'Interests',
+    text: 'These are my interests',
+  },
+];
+
+export { sections };
