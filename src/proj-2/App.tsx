d2dfa47ddc5e4cc24439946e53ca9bfb0bc9e0a2
Philipp Khromov <filooloog@mail.ru>
2022-12-16 19:50:02 +0300

    feat: add course Cards mapping

--- src/proj-2/App.tsx
+++ src/proj-2/App.tsx
@@ -1,12 +1,24 @@
 import classes from './App.module.scss';
 import Header from './commons/Header';
 import Hero from './commons/Hero';
+import { courseCards } from './config';
+import Card from './commons/Card';
 
 function App() {
+  const renderCourseCards = () => {
+    const cards = courseCards.map((courseData) => {
+      const { imageAlt } = courseData;
+
+      return <Card key={imageAlt} {...courseData} />;
+    });
+
+    return <div className={classes.cardsContainer}>{cards}</div>;
+  };
   return (
     <div className={classes.app}>
       <Header />
       <Hero />
+      {renderCourseCards()}
     </div>
   );
 }
---
+++ src/proj-2/config.ts
@@ -0,0 +1,33 @@
+import KatieZafares from '../assets/airbnb/zafares.png';
+import Wedding from '../assets/airbnb/wedding.png';
+import MountainBike from '../assets/airbnb/mountain-bike.png';
+
+export const courseCards = [
+  {
+    image: KatieZafares,
+    imageAlt: 'Katie Zafares',
+    rating: '5.0',
+    feedbackCount: 6,
+    country: 'USA',
+    caption: 'Life lessons with Katie Zaferes',
+    startingPrice: 136,
+  },
+  {
+    image: Wedding,
+    imageAlt: 'A bride',
+    rating: '5.0',
+    feedbackCount: 30,
+    country: 'USA',
+    caption: 'Learn wedding photography',
+    startingPrice: 125,
+  },
+  {
+    image: MountainBike,
+    imageAlt: 'A mountain bike',
+    rating: '4.8',
+    feedbackCount: 2,
+    country: 'USA',
+    caption: 'Group mountain biking',
+    startingPrice: 50,
+  },
+];
