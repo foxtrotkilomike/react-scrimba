4444cc4319678c23e2b19fb7b03b89e7fb1a45f6
Philipp Khromov <filooloog@mail.ru>
2022-12-18 12:13:20 +0300

    init: proj-3

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './proj-2';
+import App from './proj-3';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
---
+++ src/proj-3/App.tsx
@@ -0,0 +1,7 @@
+import classes from './App.module.scss';
+
+function App() {
+  return <div className={classes.app}></div>;
+}
+
+export default App;
