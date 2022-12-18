7f17545955017de70891161160aa8510e3afad94
Philipp Khromov <filooloog@mail.ru>
2022-12-18 14:09:41 +0300

    feat: add ArticlePreview with header & image

--- src/proj-3/App.tsx
+++ src/proj-3/App.tsx
@@ -1,10 +1,13 @@
 import classes from './App.module.scss';
 import Header from './commons/Header';
+import ArticlePreview from './commons/ArticlePreview';
+import { articles } from './config';
 
 function App() {
   return (
     <div className={classes.app}>
       <Header />
+      <ArticlePreview {...articles[0]} />
     </div>
   );
 }
---
+++ src/proj-3/config.ts
@@ -0,0 +1,12 @@
+export const articles = [
+  {
+    title: 'Mount Fuji',
+    location: 'Japan',
+    googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
+    startDate: new Date('12/12/22'),
+    endDate: new Date('18/12/22'),
+    description:
+      'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+    imageUrl: 'https://source.unsplash.com/WLxQvbMyfas',
+  },
+];
