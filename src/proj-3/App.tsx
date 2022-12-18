48cedbad34d1ffab122873f7c5f8d16737b58546
Philipp Khromov <filooloog@mail.ru>
2022-12-18 13:30:53 +0300

    feat: add Header with centered text & icon

--- src/proj-3/App.tsx
+++ src/proj-3/App.tsx
@@ -1,7 +1,12 @@
 import classes from './App.module.scss';
+import Header from './commons/Header';
 
 function App() {
-  return <div className={classes.app}></div>;
+  return (
+    <div className={classes.app}>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
