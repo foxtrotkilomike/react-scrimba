2e8bc9bea2ceed9847f13ddae4c75267018bb9ab
Philipp Khromov <filooloog@mail.ru>
2022-12-22 17:15:51 +0300

    feat: map travel data to Articles preview with hr

--- src/proj-3/App.tsx
+++ src/proj-3/App.tsx
@@ -1,15 +1,24 @@
+import React from 'react';
 import classes from './App.module.scss';
 import Header from './commons/Header';
 import ArticlePreview from './commons/ArticlePreview';
 import { articles } from './config';
 
 function App() {
+  const renderArticlePreviews = () =>
+    articles.map((article, index) => {
+      return (
+        <React.Fragment key={article.googleMapsUrl}>
+          <ArticlePreview {...article} />
+          {index === articles.length - 1 ? null : <hr className={classes.hr} />}
+        </React.Fragment>
+      );
+    });
+
   return (
     <div className={classes.app}>
       <Header />
-      <div className={classes.articlesContainer}>
-        <ArticlePreview {...articles[0]} />
-      </div>
+      <div className={classes.articlesContainer}>{renderArticlePreviews()}</div>
     </div>
   );
 }
--- src/proj-3/config.ts
+++ src/proj-3/config.ts
@@ -18,28 +18,28 @@ export const articles: ArticlePreviewProps[] = [
     imageUrl: StPetersburgPhoto,
   },
   {
-    title: 'Yeltsin Centre',
+    title: 'Yeltsin Center',
     location: 'Ekaterinburg, Russia',
-    googleMapsUrl: 'https://goo.gl/maps/aMjgvCrQn3a2C3TE7',
+    googleMapsUrl: 'https://goo.gl/maps/pYggzifVpf1ditKcA',
     startDate: new Date('06/27/22'),
     endDate: new Date('06/29/22'),
     description:
-      'Yeltsin Centre is a modern public space, which is most famous for the' +
+      'Yeltsin Center is a modern public space, which is most famous for the' +
       ' museum of Boris Yeltsin. It exposes a life and career of the first' +
-      ' Russian president in a form of several key moments he and his' +
-      ' country faced during the 20th century',
+      ' Russian president in a form of several key events he and his' +
+      ' country faced in the 20th century',
     imageUrl: EkaterinburgPhoto,
   },
   {
     title: 'Arkhyz',
     location: 'Karachaevo-Cherkessiya, Russia',
-    googleMapsUrl: 'https://goo.gl/maps/aMjgvCrQn3a2C3TE7',
+    googleMapsUrl: 'https://goo.gl/maps/zxkh7u7WibLi1dus6',
     startDate: new Date('01/20/22'),
     endDate: new Date('01/27/22'),
     description:
       'Arkhyz is famous for its ski resort and mild climate, which lets one' +
       ' enjoy winter in all its beauty. It was exciting time with Igor and I' +
-      ' managed to beat my speed record (73.1 km/h',
+      ' managed to beat my speed record (73.1 km/h)',
     imageUrl: ArkhyzPhoto,
   },
 ];
